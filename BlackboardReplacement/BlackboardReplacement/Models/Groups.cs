9413295a67b19755fb4cd4a9c2203ec27daed50a
Jannik <Jannik.lading@hotmail.dk>
2019-04-04 15:59:23 +0200

    StudentGroups

--- BlackboardReplacement/BlackboardReplacement/Models/Groups.cs
+++ BlackboardReplacement/BlackboardReplacement/Models/Groups.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BlackboardReplacement.Models
 {
     public class Groups
     {
+
+        [Key]
+        public int GroupId { get; set; }
+
+        public int maxSize { get; set; }
+
     }
 }
