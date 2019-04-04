856bf68c5f9e36364bcc170697a656ae30ffea5a
Jannik <Jannik.lading@hotmail.dk>
2019-04-04 14:30:01 +0200

    Student added

--- BlackboardReplacement/BlackboardReplacement/Models/Student.cs
+++ BlackboardReplacement/BlackboardReplacement/Models/Student.cs
@@ -1,7 +1,30 @@
-﻿namespace BlackboardReplacement.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
+
+
+namespace BlackboardReplacement.Models
 {
     public class Student
     {
-        
+        [Key]
+        public string AUID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        public DateTime EnrolledDate { get; set; }
+
+        [Required]
+        public DateTime GraduationDate { get; set; }
+
+
+
     }
 }
