d2897c62d72547350a43778b66244dee672df414
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2019-04-04 14:28:06 +0200

    CourseContent entities implemented

---
+++ BlackboardReplacement/BlackboardReplacement/Models/CourseContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackboardReplacement.Models
+{
+    class CourseContent
+    {
+        public string Audio { get; set; }
+        public string Video { get; set; }
+        public string TextBlock { get; set; }
+        public string Folder { get; set; }
+
+    }
+}
