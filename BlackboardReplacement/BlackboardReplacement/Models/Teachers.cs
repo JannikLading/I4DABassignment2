1218794cbb0636d48671a251f64cf652704475d5
Nichlaes <nichlaes.soerensen@gmail.com>
2019-04-04 15:22:22 +0200

    teachers implemented without keys

--- BlackboardReplacement/BlackboardReplacement/Models/Teachers.cs
+++ BlackboardReplacement/BlackboardReplacement/Models/Teachers.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
 
 namespace BlackboardReplacement.Models
 {
-    class Teachers
+    public class Teachers
     {
+        [Key]
+        public int auID { get; set; }
+
+        public int courseID { get; set; }
+
+        [Required]
+        public string name { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        public Courses courses { get; set; }
+
     }
 }
